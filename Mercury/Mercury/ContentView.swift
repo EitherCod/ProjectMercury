7467bdab12e38286eea5cc9ef8bff4bc685480d9
Vinay <vchadala@berkeley.edu>
2020-04-04 00:16:04 -0700

    base files

---
+++ Mercury/Mercury/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mercury
+//
+//  Created by Vinay Chadalavada on 4/4/20.
+//  Copyright © 2020 Vinay Chadalavada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
